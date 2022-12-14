62236e75a5b262c232b88d3cb9fe67af0c68a608
Allard Oelen <oelen@l3s.de>
2022-12-14 18:25:12 +0100

    initial release of version 0.1.0

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
-# <img src="public/icons/icon_48.png" width="45" align="left"> Who Extension
+#<img src="public/icons/icon.png" width="45" align="left" style="margin-right: 10px"> WHO COVID-19 Research Database Extension
 
-My Chrome Extension
+This Chrome extension enhances the [WHO COVID-19 Research Database](https://search.bvsalud.org/global-literature-on-novel-coronavirus-2019-ncov/) by providing additional labels to articles listed in the interface. See the labels next to the article titles in the screenshot below.
+
+<img src="docs/img/screenshot.png" width="800">
 
 ## Features
 
-- Feature 1
-- Feature 2
+Additional labels are displayed including:
+
+- Influential level: either high or low
+- Citation count
+- Altmetric
+- Article topic
 
 ## Install
 
-[**Chrome** extension]() <!-- TODO: Add chrome extension link inside parenthesis -->
+The extension is currently not yet available in the Chrome store. You can download the source of this project, and load it manually. Also make sure the paper classifier is running on `http://127.0.0.1:8000/`. (More information about the paper classifier will be added later.)
 
-## Contribution
+1. Clone this repository
+2. Visit `chrome://extensions` in the browser
+3. Click on the `Load unpacked` button
+   <img src="docs/img/loadunpacked.png" width="400">
+4. Find the `build` directory in the just cloned repository and select it. The extension should be visible now in the extension list:
+   <img src="docs/img/extension.png" width="400">
+5. Visit the [WHO COVID-19 Research Database](https://search.bvsalud.org/global-literature-on-novel-coronavirus-2019-ncov/) (the extension is only activated on this website). The extension will be visible as displayed below. Additionally, the article titles in the search interface will include additional labels.
+   <img src="docs/img/tooltip.png" width="400">
 
-Suggestions and pull requests are welcomed!.
+## Development
+
+Ensure you have [node](https://nodejs.org/en/) (>16) and npm installed.
+
+1. Perform the steps above to install the extension
+2. Within the root directory of this plugin, run `npm install`
+3. Afterwards run `npm run watch` to run the development server
+4. When you make a change to the code, by sure to click the 'reload' button next to the extension on the extension page: `chrome://extensions`
 
 ---
 
 This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli)
-
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,106 @@
 'use strict';
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+const API_PAPER_CLASSIFICATION = 'http://127.0.0.1:8000/';
+const API_OPEN_CITATIONS = 'https://opencitations.net/index/api/v1/';
+const API_ALT_METRIC = 'https://api.altmetric.com/v1/doi/';
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+const templateInfluentialLabel = ({ level }) =>
+  level === 'high' || level === 'low'
+    ? `<span style="background: ${
+        level === 'high' ? 'red' : 'grey'
+      }; color: white; padding: 5px; font-size: 80%; margin-right:5px;white-space: nowrap; margin: 0 5px">impact: ${level}</span>`
+    : '';
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
+const templateCitationLabel = ({ citations }) =>
+  `<span style="background: grey; color: white; padding: 5px;font-size: 80%;white-space: nowrap; margin: 0 5px">citations: ${citations}</span>`;
 
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
+const templateLabel = ({ label }) =>
+  `<span style="background: grey; color: white; padding: 5px;font-size: 80%;white-space: nowrap; margin: 0 5px">topic: ${label
+    .toLowerCase()
+    .replace('_', ' ')}</span>`;
 
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
+const templateAltMetric = ({ img, url }) =>
+  `<a href="${url}" target="_blank"><img src="${img}" style="width:35px; margin: 0 5px"/></a>`;
+
+const templatePlaceholder = () =>
+  '<span data-id="influential-label"><img src="https://cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif" width="15" /></span><span data-id="label"></span><span data-id="category"></span><span data-id="citations"></span><span data-id="altmetric"></span>';
+
+const fetchPaperData = (title) => {
+  return fetch(
+    `${API_PAPER_CLASSIFICATION}WHO_paper_classification/research_paper_classification_using_title_UncataData?title=${title}&UploadPdf=False`,
+    { method: 'POST' }
+  )
+    .then((response) => response.json())
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const fetchOpenCitations = (doi) => {
+  return fetch(`${API_OPEN_CITATIONS}citation-count/${doi}`)
+    .then((response) => response.json())
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const fetchAltMetricBadgeUrl = (doi) => {
+  return fetch(`${API_ALT_METRIC}${doi}`)
+    .then((response) => response.json())
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const getPapers = () =>
+  [...document.querySelectorAll('.titleArt')].map((paper) => ({
+    element: paper,
+    title: paper.innerText,
+  }));
+
+const addLabel = ({ template, appendTo }) => {
+  appendTo.innerHTML = template;
+};
+
+const displayImpactLabels = () => {
+  for (const { element, title } of getPapers()) {
+    const badgesPlaceholder = document.createElement('span');
+    badgesPlaceholder.innerHTML = templatePlaceholder();
+    element.append(badgesPlaceholder);
+
+    fetchPaperData(title).then((paperData) => {
+      fetchOpenCitations(paperData.DOI).then((openCitations) => {
+        addLabel({
+          template: templateCitationLabel({
+            citations: openCitations[0].count,
+          }),
+          appendTo: element.querySelector('[data-id="citations"]'),
+        });
+      });
+      fetchAltMetricBadgeUrl(paperData.DOI).then((data) => {
+        addLabel({
+          template: templateAltMetric({
+            img: data.images.small,
+            url: data.details_url,
+          }),
+          appendTo: element.querySelector('[data-id="altmetric"]'),
+        });
+      });
+      addLabel({
+        template: templateInfluentialLabel({
+          level: paperData['Influential Level'],
+        }),
+        appendTo: element.querySelector('[data-id="influential-label"]'),
+      });
+      addLabel({
+        template: templateLabel({
+          label: paperData.Label,
+        }),
+        appendTo: element.querySelector('[data-id="label"]'),
+      });
+    });
   }
-);
+};
 
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
+console.log('Starting the widget...');
+displayImpactLabels();
